7cf4b53cf6ae1eb21ef47f25aea1b6b502ccf5a8
JKKook <jk5558@naver.com>
2022-06-15 15:14:52 +0900

    ALL.realignment

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ navbarMenu.addEventListener('click', (event) => {
     scrollIntoView(link);
 });
 
-
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
@@ -57,7 +56,6 @@ document.addEventListener('scroll', () => {
  }
 });
 
-
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
@@ -67,3 +65,28 @@ function scrollIntoView(selector) {
 arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 });
+
+// Projects
+const workBtnContainer = document.querySelector('work__categories');
+const projectContainer = document.querySelector('work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    console.log(filter);
+    projects.forEach((project) => {
+    console.log(project.dataset.type);
+        if (filter === '*' || filter === project.dataset.type) {
+            project.classList.remove('invisible');
+        } else {
+            project.classList.add('invisible');
+        }
+    });
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
